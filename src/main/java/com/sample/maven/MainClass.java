04c475f76fcaa6f0d74d23f15d847a06c4a49b9a
Muniraj <p.munirajkumar@gmail.com>
2021-05-31 23:21:33 +0530

    My first commit

---
+++ src/main/java/com/sample/maven/MainClass.java
@@ -0,0 +1,11 @@
+package com.sample.maven;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HElloooo ! This is my sample maven project.");
+
+	}
+
+}
